75f2a4ed28c35aa7d8fa1886d7c2635e70b9a337
NoahBroJ <noahzarkbro@gmail.com>
2017-05-29 18:55:14 +0200

    Improved effects on navbar links.

--- index.php
+++ index.php
@@ -25,13 +25,28 @@
         <main>
             <div class="article-wrapper">
                 <article>
-                    <h1>
-                        [Article Header]
-                    </h1>
-                    <img src="img/shark1.jpg" alt="Smiley Sharkey">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime praesentium minima voluptate veritatis quas sunt, numquam nisi iste optio libero est, similique aut sapiente nam recusandae ut a. Minima, eveniet.</p>
-                    <p>Tempore, consequatur placeat excepturi animi illum odio non quaerat eius cumque! Porro dolorem vitae expedita, reprehenderit similique fugit molestiae animi praesentium soluta, est perspiciatis consequuntur corporis ducimus maxime. Repellat, reiciendis.</p>
-                    <p>Rerum, totam earum, quod aspernatur doloremque nemo tempora aut dicta, laudantium voluptatum cumque odio sint sit veritatis vel nihil laboriosam ut ipsam maxime consequuntur pariatur. Tempore voluptatem dicta aliquid recusandae?</p>
+                    <h1>Article 1</h1>
+                    <img src="img/shark1.jpg" alt="">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus voluptates, quis cumque? Omnis vel nostrum aspernatur, optio libero autem enim eveniet ut. Voluptatibus neque quidem et debitis modi, quam ex.</p>
+                    <p>Iure ipsum, quasi assumenda distinctio asperiores et ducimus temporibus? Modi hic quaerat dicta laborum autem fuga, accusamus rem provident maxime delectus sequi sapiente odit quos facilis soluta. Quidem, necessitatibus, suscipit.</p>
+                    <p>Laborum tempore, sit pariatur iure explicabo! Deleniti assumenda, cum iure commodi itaque quam, dolores magni officiis, quisquam, ullam possimus rem pariatur distinctio voluptate quod adipisci illo voluptatibus natus dicta ipsum.</p>
+                </article>
+                <article>
+                    <h1>Article 2</h1>
+                    <img src="img/shark2.jpg" alt="">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti est voluptatum, tenetur unde id quae cum impedit dolore ex reprehenderit expedita, enim obcaecati sequi autem sit nulla. Beatae, libero fuga.</p>
+                    <p>Repudiandae nam vitae recusandae exercitationem ipsa! Quidem commodi, dolores deleniti reiciendis doloremque atque assumenda iure nobis natus eaque enim quos, illo corporis esse facilis earum laboriosam necessitatibus quisquam consequatur, neque.</p>
+                    <p>Labore neque nisi natus quis saepe consectetur? Quos sunt accusantium quod repellat delectus optio sed amet architecto eos quidem blanditiis doloremque repellendus similique odio, cum voluptatem quasi nostrum praesentium hic!</p>
+                    <p>Harum molestiae esse, architecto, ducimus minus libero placeat eum doloremque in facilis vitae provident fugiat id tempore rerum sed. Odit sapiente pariatur consequatur possimus delectus cumque, soluta quam veritatis fuga.</p>
+                    <p>Culpa consequuntur, minima, nihil quis atque veniam officiis, repellat aut neque fugit delectus, vel cum! Cumque voluptatum architecto velit dolorum delectus atque quaerat aut! Labore sint dolorem perspiciatis, quibusdam dolorum.</p>
+                </article>
+                <article>
+                    <h1>Article 3</h1>
+                    <img src="img/shark3.jpg" alt="">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi quam deserunt asperiores deleniti atque adipisci assumenda, voluptatibus eius fugiat exercitationem illum, quas et delectus cumque molestiae corporis animi minima iure.</p>
+                    <p>Soluta, fugiat similique! Magni eum expedita provident adipisci maxime possimus reprehenderit, iusto ab porro laboriosam beatae, ut sequi alias laudantium minima illo ex dolores nulla et nesciunt. Dolore, sed, quae?</p>
+                    <p>Neque inventore eius, ipsam autem expedita, ad eos laudantium cupiditate deserunt repellat. Nostrum odit odio alias eligendi ipsum expedita corrupti eius pariatur aliquid, cumque non, ratione nesciunt iste doloribus dolore.</p>
+                    <p>Velit quos maxime eos, itaque ducimus sequi consequuntur harum autem quis aperiam! Laboriosam itaque numquam voluptatum, officiis quae libero at nam, molestias accusantium deserunt sapiente nostrum magni quisquam tempora consequatur!</p>
                 </article>
             </div>
         </main>
