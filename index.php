0fee1e282130d5a4f13f5f134525f7d10f3bcb04
NoahBroJ <noahzarkbro@gmail.com>
2017-05-29 15:01:14 +0200

    added articles

--- index.php
+++ index.php
@@ -23,7 +23,17 @@
         </header>
         
         <main>
-            
+            <div class="article-wrapper">
+                <article>
+                    <h1>
+                        [Article Header]
+                    </h1>
+                    <img src="img/shark1.jpg" alt="Smiley Sharkey">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime praesentium minima voluptate veritatis quas sunt, numquam nisi iste optio libero est, similique aut sapiente nam recusandae ut a. Minima, eveniet.</p>
+                    <p>Tempore, consequatur placeat excepturi animi illum odio non quaerat eius cumque! Porro dolorem vitae expedita, reprehenderit similique fugit molestiae animi praesentium soluta, est perspiciatis consequuntur corporis ducimus maxime. Repellat, reiciendis.</p>
+                    <p>Rerum, totam earum, quod aspernatur doloremque nemo tempora aut dicta, laudantium voluptatum cumque odio sint sit veritatis vel nihil laboriosam ut ipsam maxime consequuntur pariatur. Tempore voluptatem dicta aliquid recusandae?</p>
+                </article>
+            </div>
         </main>
         <aside>
             
