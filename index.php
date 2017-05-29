6f0412f22904a06e5dcbed116e7defa74d388bd6
NoahBroJ <noahzarkbro@gmail.com>
2017-05-29 14:15:59 +0200

    Created index, style, and image files

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Tsundere Sharks</title>
+    
+    <link rel="stylesheet" href="styles/style.css">
+</head>
+<body>
+    <div id="wrapper">
+        <header>
+            <div class="side-frame"></div>
+            <div class="picture-frame">
+                <img src="img/hShark.jpg" alt="Big Tsundere">
+            </div>
+            <div class="side-frame"></div>
+            <nav>
+                <div class="link-item"><a href="#">Kyaaaa!</a></div>
+                <div class="link-item"><a href="#">B-Baka!</a></div>
+                <div class="link-item"><a href="#">Senpai!</a></div>
+                <div class="link-item"><a href="#">Nomnom!</a></div>
+            </nav>
+        </header>
+        
+        <main>
+            
+        </main>
+        <aside>
+            
+        </aside>
+        
+        <footer>
+            
+        </footer>
+    </div>
+</body>
+</html>
