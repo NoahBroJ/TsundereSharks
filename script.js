98d7509fff8713cb96bde61374ea1f0936dda861
NoahBroJ <noahzarkbro@gmail.com>
2017-05-31 21:00:15 +0200

    Added a scrollTo function to link sidebar articles and main feed articles

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ $(document).ready(function() {
     $("#recent-posts > article").on("click", function() {
         var articleId = "#" + $(this).data("articleid");
         var article = $(articleId);
-        (window).scrollTo(0, article.scrollTop());
-    })
-})
+        $("html, body").animate({
+            scrollTop: article.offset().top;
+        }, 400);
+    });
+});
