eacffe4f86db5f10a4eb058ae327276c8b981aaa
NoahBroJ <noahzarkbro@gmail.com>
2017-05-31 10:59:54 +0200

    Finished index and created login page

--- index.php
+++ index.php
@@ -4,7 +4,11 @@
     <meta charset="UTF-8">
     <title>Tsundere Sharks</title>
     
-    <link rel="stylesheet" href="styles/style.css">
+    <link rel="stylesheet" href="styles/master-style.css">
+    <link rel="stylesheet" href="styles/index-style.css">
+    
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="script.js"></script>
 </head>
 <body>
     <div id="wrapper">
@@ -15,10 +19,10 @@
             </div>
             <div class="side-frame"></div>
             <nav>
-                <div class="link-item"><a href="#">Kyaaaa!</a></div>
+                <div class="link-item active-link"><a href="#">Kyaaaa!</a></div>
                 <div class="link-item"><a href="#">B-Baka!</a></div>
                 <div class="link-item"><a href="#">Senpai!</a></div>
-                <div class="link-item"><a href="#">Nomnom!</a></div>
+                <div class="link-item"><a href="#">Login/Sign up</a></div>
             </nav>
         </header>
         
@@ -26,33 +30,38 @@
             <div class="article-wrapper">
                 <article>
                     <h1>Article 1</h1>
-                    <img src="img/shark1.jpg" alt="">
+                    <img class="article-pic" src="img/shark1.jpg" alt="">
                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus voluptates, quis cumque? Omnis vel nostrum aspernatur, optio libero autem enim eveniet ut. Voluptatibus neque quidem et debitis modi, quam ex.</p>
                     <p>Iure ipsum, quasi assumenda distinctio asperiores et ducimus temporibus? Modi hic quaerat dicta laborum autem fuga, accusamus rem provident maxime delectus sequi sapiente odit quos facilis soluta. Quidem, necessitatibus, suscipit.</p>
                     <p>Laborum tempore, sit pariatur iure explicabo! Deleniti assumenda, cum iure commodi itaque quam, dolores magni officiis, quisquam, ullam possimus rem pariatur distinctio voluptate quod adipisci illo voluptatibus natus dicta ipsum.</p>
+                    <footer>
+                        <div class="author-box">
+                            <img src="img/shark3.jpg" alt="" class="profile-pic">
+                            <i class="author">[Author name]</i>
+                        </div>
+                        <i class="time-stamp">DD-MM-YYYY</i>
+                    </footer>
                 </article>
                 <article>
                     <h1>Article 2</h1>
-                    <img src="img/shark2.jpg" alt="">
+                    <img class="article-pic" src="img/shark2.jpg" alt="">
                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti est voluptatum, tenetur unde id quae cum impedit dolore ex reprehenderit expedita, enim obcaecati sequi autem sit nulla. Beatae, libero fuga.</p>
                     <p>Repudiandae nam vitae recusandae exercitationem ipsa! Quidem commodi, dolores deleniti reiciendis doloremque atque assumenda iure nobis natus eaque enim quos, illo corporis esse facilis earum laboriosam necessitatibus quisquam consequatur, neque.</p>
                     <p>Labore neque nisi natus quis saepe consectetur? Quos sunt accusantium quod repellat delectus optio sed amet architecto eos quidem blanditiis doloremque repellendus similique odio, cum voluptatem quasi nostrum praesentium hic!</p>
                     <p>Harum molestiae esse, architecto, ducimus minus libero placeat eum doloremque in facilis vitae provident fugiat id tempore rerum sed. Odit sapiente pariatur consequatur possimus delectus cumque, soluta quam veritatis fuga.</p>
                     <p>Culpa consequuntur, minima, nihil quis atque veniam officiis, repellat aut neque fugit delectus, vel cum! Cumque voluptatum architecto velit dolorum delectus atque quaerat aut! Labore sint dolorem perspiciatis, quibusdam dolorum.</p>
-                </article>
-                <article>
-                    <h1>Article 3</h1>
-                    <img src="img/shark3.jpg" alt="">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi quam deserunt asperiores deleniti atque adipisci assumenda, voluptatibus eius fugiat exercitationem illum, quas et delectus cumque molestiae corporis animi minima iure.</p>
-                    <p>Soluta, fugiat similique! Magni eum expedita provident adipisci maxime possimus reprehenderit, iusto ab porro laboriosam beatae, ut sequi alias laudantium minima illo ex dolores nulla et nesciunt. Dolore, sed, quae?</p>
-                    <p>Neque inventore eius, ipsam autem expedita, ad eos laudantium cupiditate deserunt repellat. Nostrum odit odio alias eligendi ipsum expedita corrupti eius pariatur aliquid, cumque non, ratione nesciunt iste doloribus dolore.</p>
-                    <p>Velit quos maxime eos, itaque ducimus sequi consequuntur harum autem quis aperiam! Laboriosam itaque numquam voluptatum, officiis quae libero at nam, molestias accusantium deserunt sapiente nostrum magni quisquam tempora consequatur!</p>
+                    <footer>
+                        <div class="author-box">
+                            <img src="img/shark4.jpg" alt="" class="profile-pic">
+                            <i class="author">[Author name]</i>
+                        </div>
+                        <i class="time-stamp">DD-MM-YYYY</i>
+                    </footer>
                 </article>
             </div>
             
             <aside>
                 <div class="login aside-box">
-                    <h2>Login</h2>
                     <form action="">
                         <div class="form-group">
                             <label for="email">Email</label>
@@ -62,6 +71,10 @@
                         <div class="form-group">
                             <label for="password">Password</label>
                             <input id="password" type="password">
+                        </div>
+                        <div class="form-group">
+                            <input class="submit" value="Login" type="submit">
+                            <input class="submit" value="Login gently" type="submit">
                         </div>
                     </form>
                 </div>
@@ -87,7 +100,7 @@
                 </div>
                 
                 <div class="news aside-box">
-                    <h2>News around the World</h2>
+                    <h2>News around the Ocean</h2>
                     <article>
                         <h3>Mini-article 1</h3>
                         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
@@ -115,10 +128,10 @@
                 </div>
             </aside>
         </main>
-        
-        <footer>
-            
-        </footer>
     </div>
+    
+    <footer class="main-footer">
+        © Noah Bro-Jørgensen | 2017
+    </footer>
 </body>
 </html>
---
+++ login.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Tsundere Sharks</title>
+    
+    <link rel="stylesheet" href="styles/master-style.css">
+    <link rel="stylesheet" href="styles/index-style.css">
+    
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="script.js"></script>
+</head>
+<body>
+    <div id="wrapper">
+        <header>
+            <div class="side-frame"></div>
+            <div class="picture-frame">
+                <img src="img/hShark.jpg" alt="Big Tsundere">
+            </div>
+            <div class="side-frame"></div>
+            <nav>
+                <div class="link-item active-link"><a href="#">Kyaaaa!</a></div>
+                <div class="link-item"><a href="#">B-Baka!</a></div>
+                <div class="link-item"><a href="#">Senpai!</a></div>
+                <div class="link-item"><a href="#">Login/Sign up</a></div>
+            </nav>
+        </header>
+        
+        <main>
+            
+        </main>
+    </div>
+    
+    <footer class="main-footer">
+        © Noah Bro-Jørgensen | 2017
+    </footer>
+</body>
+</html>
